ff4bd66647e26b5fa9021f1d4b0d7130fec7e0db
Pablo Pérez González <alu0101318318@ull.edu.es>
2024-04-25 02:18:24 +0100

    No time.sleep, espera y tarda el tiempo exacto  -- Se implementa tratamiento de imagenes

--- App Flask/app.py
+++ App Flask/app.py
@@ -1,174 +1,197 @@
-from flask import Flask, render_template, jsonify, send_file
-from flask_socketio import SocketIO, emit
+from image_treatment import imageTreatment
+from flask import Flask, render_template, jsonify, send_file, request
+from flask_cors import CORS, cross_origin
+
 import flask
 import json
+import logging
 import urllib
 import time
 
-import logging
-
+# Logger config
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
+# Creating the app
 app = Flask(__name__)
-socketio = SocketIO(app)
+CORS(app) # Habilitar CORS para toda la aplicación
 
 DEFAULT_PROMPT_JSON = """
 {
-  "3": {
-    "inputs": {
-      "seed": 1046630439646439,
-      "steps": 20,
-      "cfg": 8,
-      "sampler_name": "euler",
-      "scheduler": "normal",
-      "denoise": 1,
-      "model": [
-        "10",
-        0
-      ],
-      "positive": [
-        "6",
-        0
-      ],
-      "negative": [
-        "7",
-        0
-      ],
-      "latent_image": [
-        "5",
-        0
-      ]
+    "3": {
+        "inputs": {
+            "seed": 1046630439646439,
+            "steps": 20,
+            "cfg": 8,
+            "sampler_name": "euler",
+            "scheduler": "normal",
+            "denoise": 1,
+            "model": [
+                "10",
+                0
+            ],
+            "positive": [
+                "6",
+                0
+            ],
+            "negative": [
+                "7",
+                0
+            ],
+            "latent_image": [
+                "5",
+                0
+            ]
+        },
+        "class_type": "KSampler",
+        "_meta": {
+        "title": "KSampler"
+        }
     },
-    "class_type": "KSampler",
-    "_meta": {
-      "title": "KSampler"
-    }
-  },
-  "4": {
-    "inputs": {
-      "ckpt_name": "v1-5-pruned-emaonly.safetensors"
+    "4": {
+        "inputs": {
+        "ckpt_name": "v1-5-pruned-emaonly.safetensors"
+        },
+        "class_type": "CheckpointLoaderSimple",
+        "_meta": {
+        "title": "Load Checkpoint"
+        }
     },
-    "class_type": "CheckpointLoaderSimple",
-    "_meta": {
-      "title": "Load Checkpoint"
-    }
-  },
-  "5": {
-    "inputs": {
-      "width": 512,
-      "height": 512,
-      "batch_size": 1
+    "5": {
+        "inputs": {
+        "width": 512,
+        "height": 512,
+        "batch_size": 1
+        },
+        "class_type": "EmptyLatentImage",
+        "_meta": {
+        "title": "Empty Latent Image"
+        }
     },
-    "class_type": "EmptyLatentImage",
-    "_meta": {
-      "title": "Empty Latent Image"
-    }
-  },
-  "6": {
-    "inputs": {
-      "text": "TFGMapPablo map of a square room where there is a very big chair at center left, a small chair at bottom right, a very big chair at top right, a big door at center right and a very big table at bottom left",
-      "clip": [
-        "10",
-        1
-      ]
+    "6": {
+        "inputs": {
+        "text": "TFGMapPablo map of a square room where there is a very big chair at center left, a small chair at bottom right, a very big chair at top right, a big door at center right and a very big table at bottom left",
+        "clip": [
+            "10",
+            1
+        ]
+        },
+        "class_type": "CLIPTextEncode",
+        "_meta": {
+        "title": "CLIP Text Encode (Prompt)"
+        }
     },
-    "class_type": "CLIPTextEncode",
-    "_meta": {
-      "title": "CLIP Text Encode (Prompt)"
-    }
-  },
-  "7": {
-    "inputs": {
-      "text": "text, watermark",
-      "clip": [
-        "10",
-        1
-      ]
+    "7": {
+        "inputs": {
+        "text": "text, watermark",
+        "clip": [
+            "10",
+            1
+        ]
+        },
+        "class_type": "CLIPTextEncode",
+        "_meta": {
+        "title": "CLIP Text Encode (Prompt)"
+        }
     },
-    "class_type": "CLIPTextEncode",
-    "_meta": {
-      "title": "CLIP Text Encode (Prompt)"
-    }
-  },
-  "8": {
-    "inputs": {
-      "samples": [
-        "3",
-        0
-      ],
-      "vae": [
-        "4",
-        2
-      ]
+    "8": {
+        "inputs": {
+        "samples": [
+            "3",
+            0
+        ],
+        "vae": [
+            "4",
+            2
+        ]
+        },
+        "class_type": "VAEDecode",
+        "_meta": {
+        "title": "VAE Decode"
+        }
     },
-    "class_type": "VAEDecode",
-    "_meta": {
-      "title": "VAE Decode"
-    }
-  },
-  "9": {
-    "inputs": {
-      "filename_prefix": "ComfyUI",
-      "images": [
-        "8",
-        0
-      ]
+    "9": {
+        "inputs": {
+        "filename_prefix": "ComfyUI",
+        "images": [
+            "8",
+            0
+        ]
+        },
+        "class_type": "SaveImage",
+        "_meta": {
+        "title": "Save Image"
+        }
     },
-    "class_type": "SaveImage",
-    "_meta": {
-      "title": "Save Image"
+    "10": {
+        "inputs": {
+        "lora_name": "lora.safetensors",
+        "strength_model": 1,
+        "strength_clip": 1,
+        "model": [
+            "4",
+            0
+        ],
+        "clip": [
+            "4",
+            1
+        ]
+        },
+        "class_type": "LoraLoader",
+        "_meta": {
+        "title": "Load LoRA"
+        }
     }
-  },
-  "10": {
-    "inputs": {
-      "lora_name": "lora.safetensors",
-      "strength_model": 1,
-      "strength_clip": 1,
-      "model": [
-        "4",
-        0
-      ],
-      "clip": [
-        "4",
-        1
-      ]
-    },
-    "class_type": "LoraLoader",
-    "_meta": {
-      "title": "Load LoRA"
-    }
-  }
 }
 """
 
-# @app.route('/img/<path:filename>') 
-# def send_file(filename): 
-#     return send_from_directory(app.upload_folder, filename)
+# Check if api works POST petition
+@app.route('/test-api', methods=['POST'])
+@cross_origin()
+def test():
+    print("Before JSON")
+    data = request.json
+    print(data)
+    return jsonify({'message': 'Imagen generada correctamente'})
+
 
 @app.route('/', methods=['GET', 'POST'])
+@cross_origin()
 def index():
     if flask.request.method == 'POST':
+        new_text = ''
         try:
-
-            new_text = flask.request.form['new_text']
+            # Se intenta obtener el prompt por petición API. Si no funciona se lee desde la web
+            try:
+                new_text = request.json
+                # print('prompt got via api')
+            except:
+                print('prompt got via web')
+                new_text = flask.request.form['new_text']
+            
             # Cargar el JSON predeterminado
             prompt = json.loads(DEFAULT_PROMPT_JSON)
+
             # Modificar la frase del prompt con la introducida por el usuario
             prompt["6"]["inputs"]["text"] = new_text
+
             # Enviar la solicitud a ComfyUI
             comfyui_response = json.loads(queue_prompt(prompt))
-            logger.info(comfyui_response)
+            # print(f'comfyui response: {comfyui_response}')
 
             prompt_id = comfyui_response['prompt_id']
-            print('prompt_id:', {prompt_id})
-            # Emitir mensaje para iniciar el procesamiento en el servidor WebSocket
-            socketio.emit('start_processing', {'prompt_id': prompt_id}, namespace='/processing')
+            print('Prompt_id:', {prompt_id})
+
             # Esperar resultados del procesamiento
             output_images = get_images(prompt_id)
 
-            # Generar una URL única para la primera imagen (puedes adaptarlo según tus necesidades)
+            # Generar una ruta única para la imagen
             first_image_url = f'static/images/{prompt_id}.jpg'
+
+            # Se obtienen los datos de la imagen
+            print(imageTreatment(first_image_url))
+
+            # Se descarga la imagen
             return send_file(first_image_url, as_attachment=True)
             # return render_template('result.html', output_images=first_image_url)
         except Exception as e:
@@ -186,27 +209,25 @@ def queue_prompt(prompt):
         response = urllib.request.urlopen(req)
 
         if response.getcode() == 200:
-            print('Response with 200 code')
-            # Acceder al contenido de la respuesta
+            print('ComfyUI response with 200 code')
             content = response.read().decode('utf-8')
-            # print(content)
             return content
         else:
             print(f"Error en la solicitud. Código de estado: {response.getcode()}")
     except Exception as e:
         return jsonify({'status': 'error', 'message': f'Error en la solicitud a ComfyUI: {str(e)}'})
 
-
+# Obtiene una imagen
 def get_image(filename, subfolder, folder_type):
     try:
         data = {"filename": filename, "subfolder": subfolder, "type": folder_type}
         url_values = urllib.parse.urlencode(data)
-        print(f'get the image: {url_values}')
         with urllib.request.urlopen(f"http://127.0.0.1:8188/view?{url_values}") as response:
             return response.read()
     except Exception as e:
         return jsonify({'status': 'error', 'message': f'Error al obtener la imagen: {str(e)}'})
 
+# Comprueba si la imagen ya ha sido generada previamente
 def get_history(prompt_id):
     try:
         with urllib.request.urlopen(f"http://127.0.0.1:8188/history/{prompt_id}") as response:
@@ -214,31 +235,37 @@ def get_history(prompt_id):
     except Exception as e:
         return jsonify({'status': 'error', 'message': f'Error al obtener el historial: {str(e)}'})
 
+# Genera la imagen
 def get_images(prompt_id):
-    output_images = {}
-    print('get_images')
-    time.sleep(30)
+    try:
+        output_images = {}
+        print('get_images')
+        # time.sleep(55)
+        while True:
+            try:
+                history = get_history(prompt_id)[prompt_id]
+                break
+            except:
+                pass
+        print('Image generated')
+        for node_id in history['outputs']:
+            node_output = history['outputs'][node_id]
+            if 'images' in node_output:
+                images_output = []
+                for i, image in enumerate(node_output['images']):
+                    print(f'Saving image')
+                    image_data = get_image(image['filename'], image['subfolder'], image['type'])
+                    images_output.append(image_data)
 
-    history = get_history(prompt_id)[prompt_id]
-    print(f'got the history')
-    for node_id in history['outputs']:
-        node_output = history['outputs'][node_id]
-        if 'images' in node_output:
-            images_output = []
-            for i, image in enumerate(node_output['images']):
-                print(f'saving image {i}')
-                image_data = get_image(image['filename'], image['subfolder'], image['type'])
-                images_output.append(image_data)
+                    # Guardar cada imagen en el servidor
+                    save_generated_image(image_data, f'{prompt_id}')
 
-                # Guardar cada imagen en el servidor (opcional)
-                save_generated_image(image_data, f'{prompt_id}')
+                output_images[node_id] = images_output
 
-            output_images[node_id] = images_output
-
-    return output_images
-
-
-
+        return output_images
+    except Exception as e:
+        return jsonify({'status': 'error', 'message': f'Error al obtener las imagenes en get_images: {str(e)}'})
+    
 def save_generated_image(image_content, filename):
     with open(f'static/images/{filename}.jpg', 'wb') as f:
         f.write(image_content)
--- App Flask/image_treatment.py
+++ App Flask/image_treatment.py
@@ -1,21 +1,23 @@
 import cv2
 
-image = cv2.imread('F:/TFG/App Flask/static/images/2b13bf54-8cbd-4a1d-bd17-f4064fe71a99.jpg')
+# image = cv2.imread('F:/TFG/App Flask/static/images/2b13bf54-8cbd-4a1d-bd17-f4064fe71a99.jpg')
 
 # Calculate the color thresholding and region props
-def imageTreatment(frame):
+def imageTreatment(filename):
+    frame = cv2.imread(filename)
+
     # The order of the colors is blue, green, red
     # floor_lower_color_bounds = (200, 0, 0)
     # floor_upper_color_bounds = (250,20,20)
-    
-    table_lower_color_bounds = (0,150,150)
-    table_upper_color_bounds = (100,255,255)
-    
-    chair_lower_color_bounds = (80,0,200)
-    chair_upper_color_bounds = (150,20,255)
-    
-    door_lower_color_bounds = (120,200,0)
-    door_upper_color_bounds = (210,255,40)
+    # Yellow
+    chair_lower_color_bounds = (0,150,150)
+    chair_upper_color_bounds = (100,255,255)
+    # Magenta
+    door_lower_color_bounds = (80,0,200)
+    door_upper_color_bounds = (150,20,255)
+    # Turquoise
+    table_lower_color_bounds = (120,200,0)
+    table_upper_color_bounds = (210,255,40)
 
     # Masks
     # floor_mask = cv2.inRange(frame, floor_lower_color_bounds, floor_upper_color_bounds)
@@ -24,9 +26,18 @@ def imageTreatment(frame):
     door_mask = cv2.inRange(frame, door_lower_color_bounds, door_upper_color_bounds)
     
     # Apply contours and centroid for each mask
-    table_regs = regionProps(table_mask.copy())
-    chair_regs = regionProps(chair_mask.copy())
-    door_regs = regionProps(door_mask.copy())
+    try:
+        table_regs = regionProps(table_mask.copy())
+    except Exception as err:
+        raise RuntimeError('Error getting region props of table', err.args)
+    try:
+        chair_regs = regionProps(chair_mask.copy())
+    except Exception as err:
+        raise RuntimeError('Error getting region props of chair', err.args)
+    try:
+        door_regs = regionProps(door_mask.copy())
+    except Exception as err:
+        raise RuntimeError('Error getting region props of door:', err.args)
     
     # cv2.imshow('table with contours', table_regs['mask'])
     # cv2.imshow('chair with contours', chair_regs['mask'])
@@ -38,7 +49,6 @@ def imageTreatment(frame):
         'doors': door_regs['data']
     }
 
-    print ('Press <q> to quit')
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     return regs_data
@@ -67,34 +77,31 @@ def regionProps(mask):
     for contour in contours:
         # Calculate area of the region
         area = cv2.contourArea(contour)
-        
-        # Calculate bounding rectangle dimensions
-        x, y, w, h = cv2.boundingRect(contour)
-        
-        # Calculate perimeter of the region
-        perimeter  = cv2.arcLength(contour, True)
-        
-        # Calculate centroid of the region
-        M = cv2.moments(contour)
-        centroid_x  = int(M["m10"] / M["m00"])
-        centroid_y  = int(M["m01"] / M["m00"])
-        
-        # Draw contour, centroid, and bounding rectangle on a copy of the original mask
-        # cv2.drawContours(mask_copy, [contour], -1, (0, 0, 255), 2)
-        cv2.circle(mask_copy, (centroid_x , centroid_y), 5, (0, 0, 255), -1)
-        cv2.rectangle(mask_copy, (x, y), (x + w, y + h), (50, 255, 50), 2)
-        contours_data.append({
-            'x': x,
-            'y': y,
-            'w': w,
-            'h': h,
-            'centroid_x ': centroid_x ,
-            'centroid_y': centroid_y
-        })
+        if area > 0:
+            # Calculate bounding rectangle dimensions
+            x, y, w, h = cv2.boundingRect(contour)
+            
+            # Calculate perimeter of the region
+            perimeter  = cv2.arcLength(contour, True)
+            
+            # Calculate centroid of the region
+            M = cv2.moments(contour)
+            centroid_x  = int(M["m10"] / M["m00"])
+            centroid_y  = int(M["m01"] / M["m00"])
+            
+            # Draw contour, centroid, and bounding rectangle on a copy of the original mask
+            # cv2.drawContours(mask_copy, [contour], -1, (0, 0, 255), 2)
+            cv2.circle(mask_copy, (centroid_x , centroid_y), 5, (0, 0, 255), -1)
+            cv2.rectangle(mask_copy, (x, y), (x + w, y + h), (50, 255, 50), 2)
+            contours_data.append({
+                'x': x,
+                'y': y,
+                'w': w,
+                'h': h,
+                'centroid_x ': centroid_x ,
+                'centroid_y': centroid_y
+            })
     return {
         'mask': mask_copy,
         'data': contours_data
         }
-
-
-print(imageTreatment(image))
